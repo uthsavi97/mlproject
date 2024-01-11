9eacd65fd31900554ebaeea25193ebd98f139751
uthsavi97 <uthsaviyp@gmail.com>
2024-01-11 22:39:36 +0530

    prediction pipeline

--- app.py
+++ app.py
@@ -21,13 +21,13 @@ def predict_datapoint():
         return render_template('home.html')
     else:
         data=CustomData(
-            gender = request.form.get('gender'),
-            race_ethnicity = request.form.get('ethnicity'),
-            parental_level_of_education = request.form.get('parental level of education'),
-            lunch = request.form.get('lunch'),
-            test_preparation_course=request.form.get('test preparation course'),
-            reading_score = float(request.form.get('writing score')),
-            writing_score = float(request.form.get('reading score'))
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
 
         )
         pred_df=data.get_data_as_data_frame()
@@ -43,5 +43,7 @@ def predict_datapoint():
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",debug=True)        
+
+
 
 
